6a2fd08d82883d77dc74447262b00c347d237c37
rengaoliang <2976988088@qq.coo>
2020-05-26 10:30:09 +0800

    添加 System.Drawing.Common依赖

---
+++ QRCoder.Core.Sample.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QRCoder.Core.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new Base64QRCode(qrCodeData);
+            var res = qrCode.GetGraphic(20);
+            System.Console.WriteLine(res.Length);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QRCoder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QRCoder.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
